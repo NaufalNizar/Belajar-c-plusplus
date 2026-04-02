8605fab62853450d72ac8f6a0918e88404d5cb0a
Muhammad Naufal Annizar <nizarnoval768@gmail.com>
2026-04-02 11:46:46 +0700

    Add files via upload

---
+++ Belah_Ketupat.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"masukan nilai : ";
+    int n;
+    cin>>n;
+
+    for(int q = 1; q <= n;q++){
+        for(int qq = q; qq <= n - 1;qq++){
+            cout<<" ";
+        }
+        for(int qw = 1; qw <= q*2-1;qw++){
+            cout<<"*";
+        }
+    cout<<endl;
+    }
+    for(int q = 1; q <= n;q++){
+        for(int qq = 1; qq <= q;qq++){
+            cout<<" ";
+        }
+        for(int qw = n*2-(q*2-1)-2; qw >= 1;qw--){
+            cout<<"*";
+        }
+    cout<<endl;
+    }
+}
