c1d8bd68fe1c861d78c78f0b2ad300577a0c02b2
Muhammad Naufal Annizar <nizarnoval768@gmail.com>
2026-04-03 15:20:28 +0700

    Add files via upload

---
+++ Garis_Silang.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout<<"Masukan nilai : ";
+    int n;
+    cin>>n;
+
+    for(int q = 1;q <= n ;q++){
+        for(int qq = 2;qq <= q;qq++){
+            cout<<" ";
+        }
+        for(int qw = n*2-(q*2-1);qw >= 1;qw--){
+            if(qw == 1 || qw == n*2-(q*2-1)){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+    cout<<endl;
+    }
+    for(int q = 1; q <= n - 1;q++){
+        for(int qq = q; qq <= n - 2;qq++){
+            cout<<" ";
+        }
+        for(int qw = 1; qw <= (q*2-1)+2;qw++){
+            if(qw == 1 || qw == (q*2-1)+2){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+   
+}
